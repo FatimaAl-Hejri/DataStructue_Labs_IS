9c19c09480892c0fe8ffbc3c51400c77426503ce
Fatima Al-Hajri <f99260758@gmail.com>
2025-11-30 09:37:52 +0300

    Second Lecture Application

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,5 +1,31 @@
 package Arrays;
 
+
+
 public class Main {
+    public static void main(String[] args) {
+        Arrayss arr= new Arrayss();
+        int []numbers={1,2,3,4,5};
+
+        arr.shiftLeft_delete(numbers,2);
+        arr.Traversal(numbers)
+
+        ///int []numbers=new int[5];
+
+        ;
+
+
+
+
+
+
+
+        //System.out.println(Arrays.toString(numbers));
+
+
+
+
+
     }
 }
+
